bd891d30bb73218db6d85fcf349fa4b60787bb79
AlexKriak <lemon1and3cherry@gmail.com>
2026-06-08 20:57:19 +0300

    create migrations

--- alembic/versions/371567bdcab3_initial_tables.py
+++ alembic/versions/371567bdcab3_initial_tables.py
@@ -1,27 +1,27 @@
-# alembic/versions/001_initial_tables.py
-"""Initial tables
+"""initial_tables
 
-Revision ID: 001
-Revises:
-Create Date: 2026-06-04 12:56:27.160782
+Revision ID: 371567bdcab3
+Revises: 
+Create Date: 2026-06-08 20:24:39.195305
 
 """
 from typing import Sequence, Union
-
+from enum import Enum
 from alembic import op
 import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '001'
-down_revision: Union[str, None] = None
+revision: str = '371567bdcab3'
+down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    # Создание схемы catalog, если не существует
+    # Создание схемы catalog и sales, если не существует
     op.execute(sa.text("CREATE SCHEMA IF NOT EXISTS catalog;"))
+    op.execute(sa.text("CREATE SCHEMA IF NOT EXISTS sales;"))
 
     # Создание таблицы product_categories
     op.create_table(
@@ -42,13 +42,6 @@ def upgrade() -> None:
         schema='catalog'
     )
 
-    # Добавление ограничения: только один центральный склад
-    op.execute(sa.text("""
-        ALTER TABLE catalog.warehouses ADD CONSTRAINT one_central_only CHECK (
-            (SELECT COUNT(*) FROM catalog.warehouses WHERE is_central = true) <= 1
-        );
-    """))
-
     # Создание таблицы products
     op.create_table(
         'products',
@@ -60,19 +53,48 @@ def upgrade() -> None:
         schema='catalog'
     )
 
+    # Создание таблицы stock
+    op.create_table(
+        'stock',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('products_id', sa.Integer, sa.ForeignKey('catalog.products.id'), nullable=False),
+        sa.Column('warehouses_id', sa.Integer, sa.ForeignKey('catalog.warehouses.id'), nullable=False),
+    )
+
+    statuses_enum = sa.Enum(
+        'unpublished',
+        'new',
+        'processing',
+        'pending',
+        'packing',
+        'shipped',
+    )
+
+    # Создание таблицы orders
+    op.create_table(
+        'orders',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('status', statuses_enum, nullable=False, default='unpublished'),
+        sa.Column('total_amount', sa.Numeric(precision=12, scale=2), nullable=False),
+        sa.Column('created_at', sa.Time(timezone=True), nullable=False),
+        sa.Column('warehouses_id', sa.Integer, sa.ForeignKey('catalog.warehouses.id'), nullable=False),
+        schema='sales'
+    )
+    pass
+
 
 def downgrade() -> None:
-    # Удаление таблицы products
+    # Удаление таблиц
     op.drop_table('products', schema='catalog')
+    op.drop_table('orders', schema='sales')
 
-    # Удаление ограничения перед удалением таблицы warehouses
-    op.execute(sa.text("ALTER TABLE catalog.warehouses DROP CONSTRAINT IF EXISTS one_central_only;"))
-
-    # Удаление таблицы warehouses
+    # Удаление таблиц
+    op.drop_table('stock', schema='sales')
     op.drop_table('warehouses', schema='catalog')
-
-    # Удаление таблицы product_categories
     op.drop_table('product_categories', schema='catalog')
+    op.drop_table('order_item', schema='sales')
 
-    # Удаление схемы catalog
+    # Удаление схем
     op.execute(sa.text("DROP SCHEMA IF EXISTS catalog CASCADE;"))
+    op.execute(sa.text("DROP SCHEMA IF EXISTS sales CASCADE;"))
+    pass
